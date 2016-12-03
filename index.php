0c21628a6348182d20c7a527775a677b9e939ca9
dcreason <dakotacreason@gmail.com>
2016-12-03 14:55:04 -0600

    Now handles errors in JS

--- index.php
+++ index.php
@@ -7,13 +7,11 @@ if(isset($_GET["from"]) && isset($_GET["addr"]) && $_GET["from"] != "" && $_GET[
 	function pooptitle(){
 		$arrayone = array("Mice","Dice","Lice","Slice","Ice","Heist","Rice","Price","Spice","Vice","Thrice","Vise","Gneiss","Splice",);
 		$arraytwo = array("Bash","Cash","Sash","Dash","Ash","Brash","Clash","Crash","Cache","Slash","Mash",);
-
 		return $arrayone[rand(0,count($arrayone)-1)]." ".$arraytwo[rand(0,count($arraytwo)-1)];
 	}
 	function html($str){
 		echo($str."\n");
 	}
-	
 	// index.php
 	if(isset($_GET["addr"]) && $_GET["addr"] != ""){
 		$addrset = true;
@@ -44,14 +42,20 @@ if(isset($_GET["from"]) && isset($_GET["addr"]) && $_GET["from"] != "" && $_GET[
 	html("<h3>Nice Hash stats kept simple.</h3>");
 	if($addrset){
 		html("<h4 id=\"addr\">".$_GET["addr"]."</h4>");
-		html("<ul id=\"youl\" style=\"list-style: none;\">");
-		html("<li>Loading...</li>");
-		html("</ul>");
+		html("<table style='margin: auto;'>");
+		html("<th>Algorithm</th>");
+		html("<th>Profitability (BTC)</th>");
+		html("<th>Profitability (USD)</th>");
+		html("<th>Speed</th>");
+		html("<tbody id='youl'>");
+		html("</tbody>");
+		html("</table>");
+		html("<p id='unpaid' style='margin: 0px 0px 0px 0px;'>Loading...</p>");
 		html("<script>");
 		html("window.onload = function(){window.ratedate = 10; checkrate();}");
 		html("function getdata(addr){");
 		html("	var oReq = new XMLHttpRequest()");
-		html("	oReq.addEventListener(\"load\", api)");
+		html("	oReq.addEventListener(\"load\", function(){dontdoonlytry(this.responseText)})");
 		html("	oReq.open(\"GET\", \"/?from=\"+(new Date().getTime() - 60000)+\"&addr=\"+addr+\"&type=json\")");
 		html("	oReq.send()");
 		html("}");
@@ -61,20 +65,30 @@ if(isset($_GET["from"]) && isset($_GET["addr"]) && $_GET["from"] != "" && $_GET[
 		html("			oReq.addEventListener(\"load\", function(){");
 		html("				var json = JSON.parse(this.responseText)");
 		html("				window.exrate = json.USD.last");
-		html("				console.log(exrate)");
 		html("				ratedate = new Date().getTime()");
 		html("				getdata(document.querySelector(\"#addr\").innerHTML)");
 		html("			});");
 		html("			oReq.open(\"GET\", \"https://blockchain.info/ticker?cors=true\")");
 		html("			oReq.send()");
 		html("		} else {");
-		html("			console.log(\"Used stored rate\")");
 		html("			getdata(document.querySelector(\"#addr\").innerHTML)");
 		html("		}");
 		html("}");
-		html("function api(){");
-		html("	var json = JSON.parse(this.responseText)");
-		html("	console.log(json)");
+		html("function dontdoonlytry(data){"); // You're right, this was added as an after thought
+		html("	try{");
+		html("		api(data)");
+		html("	}");
+		html("	catch(err){");
+		html("		if(err){");
+		html("			console.log(err)");
+		html("			console.log('Ouch!')");
+		html("			setTimeout(dontdoonlytry, 5000)");
+		html("		}");
+		html("	}");
+		html("}");
+		html("function api(data){");
+		html("	var totalprofitability = 0");
+		html("	var json = JSON.parse(data)");
 		html("	if(typeof json['result']['error'] == 'string'){");
 		html("		document.querySelector('#youl').innerHTML = \"<li>json['result']['error']</li>\"");
 		html("	} else {");
@@ -85,16 +99,14 @@ if(isset($_GET["from"]) && isset($_GET["addr"]) && $_GET["from"] != "" && $_GET[
 		html("		if(algo[\"data\"][1] !== '0'){");
 		html("			unpaid += parseFloat(algo[\"data\"][1])");
 		html("		}");
-		html("			console.log((algo[\"data\"][1]))");
 		html("		if(algo[\"data\"][0][\"a\"]){");
 		html("			var profitability = algo[\"data\"][0][\"a\"]*algo[\"profitability\"]");
-		html("			console.log(\"Profitability: \"+profitability)");
-		// html("			var speed = algo[\"data\"][0][\"a\"] + \" \"+algo[\"suffix\"]");
-		html("			document.querySelector(\"#youl\").innerHTML += \"<li>\"+algo[\"name\"]+\": \"+profitability+\" BTC/day | \"+(profitability * exrate).toFixed(2) +\" USD/day | \"+ algo[\"data\"][0][\"a\"] + \" \"+algo[\"suffix\"] +\"/s</li>\"");
+		html("			totalprofitability += profitability");
+		html("			document.querySelector(\"#youl\").innerHTML += \"<tr><td>\"+algo[\"name\"]+\"</td><td>\"+profitability.toFixed(6)+\" BTC/day</td><td>\"+(profitability * exrate).toFixed(2) +\" USD/day</td><td>\"+ algo[\"data\"][0][\"a\"] + \" \"+algo[\"suffix\"] +\"/s</td></tr>\"");
 		html("		}");
 		html("		if(index == arr.length -1){");
-		html("			console.log('unpaid: '+unpaid)");
-		html("			document.querySelector(\"#youl\").innerHTML += \"<li>Unpaid balance: \"+unpaid+\" | \"+(unpaid*exrate).toFixed(2)+\" USD</li>\"");
+		html("			document.querySelector(\"#youl\").innerHTML += '<tr><td>Total</td><td>'+totalprofitability.toFixed(8)+' BTC/day</td><td>'+(totalprofitability*exrate).toFixed(2)+' USD/day</td><td></td></tr>'");
+		html("			document.querySelector(\"#unpaid\").innerHTML = \"Unpaid balance: \"+unpaid.toFixed(8)+\" | \"+(unpaid*exrate).toFixed(2)+\" USD\"");
 		html("			var update = setTimeout(checkrate, 20000)");
 		html("		}");
 		html("	})");
@@ -108,7 +120,6 @@ if(isset($_GET["from"]) && isset($_GET["addr"]) && $_GET["from"] != "" && $_GET[
 		html("<script>");
 		html("document.querySelector('#go').addEventListener('click', gotem)");
 		html("function gotem(){");
-		html("	console.log('click')");
 		html("	window.location='/?addr='+document.querySelector('#addrbox').value ");
 		html("}");
 		html("</script>");
